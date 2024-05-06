530c63a0990860ca42b6675653ddd632b6b62221
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-05-06 16:54:10 +0200

    minor adjstuments

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -9,7 +9,13 @@ const Job = ({ data }) => {
   return (
     <Row className='mx-0 mt-3 p-3' style={{ border: "1px solid #00000033", borderRadius: 4 }}>
       <Col xs={3}>
-        <div className='d-flex align-items-center'>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={9}>
+        <div className='d-flex align-items-center justify-content-between'>
+          <a href={data.url} target='_blank' rel='noreferrer'>
+            {data.title}
+          </a>
           {favourites && favourites.includes(data.company_name) ? (
             <i
               className='bi bi-star-fill'
@@ -18,13 +24,7 @@ const Job = ({ data }) => {
           ) : (
             <i className='bi bi-star' onClick={() => dispatch({ type: "ADD_TO_FAVOURITES", payload: data.company_name })} />
           )}
-          <Link to={`/${data.company_name}`}>{data.company_name}</Link>
         </div>
-      </Col>
-      <Col xs={9}>
-        <a href={data.url} target='_blank' rel='noreferrer'>
-          {data.title}
-        </a>
       </Col>
     </Row>
   )
