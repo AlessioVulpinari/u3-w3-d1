56858bd64585b1cfb902304a66f50af0e2c3cc6f
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-05-06 17:17:42 +0200

    added remove favorites from favorites page

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,17 +1,22 @@
-import { Container, Row } from "react-bootstrap"
-import { useSelector } from "react-redux"
+import { Col, Container, Row } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
 
 const Favourites = () => {
   const favourites = useSelector((state) => state.favourites.content)
-
+  const dispatch = useDispatch()
   return (
     <Container>
       <Row>
         {favourites.map((companyName) => (
-          <Link to={`/${companyName}`} as='Col' key={companyName} className='col-12 border border-primary my-1 py-1'>
-            {companyName}
-          </Link>
+          <Col key={companyName} xs={12} className='border border-primary my-1 py-1 d-flex justify-content-between'>
+            <Link to={`/${companyName}`}>{companyName}</Link>
+            {favourites && favourites.includes(companyName) ? (
+              <i className='bi bi-star-fill' onClick={() => dispatch({ type: "REMOVE_FROM_FAVOURITES", payload: companyName })} />
+            ) : (
+              <i className='bi bi-star' onClick={() => dispatch({ type: "ADD_TO_FAVOURITES", payload: companyName })} />
+            )}
+          </Col>
         ))}
       </Row>
     </Container>
